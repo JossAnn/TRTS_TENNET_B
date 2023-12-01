151e2f29e860685fb87c8571b96e4ef998e28264
Joss <jnatarenordonez@gmail.com>
2023-12-01 02:14:37 -0600

    YA!!
    Ahora si Repo limpia

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import signale from "signale";
+import cors from 'cors';
+import { createTables } from "./database/mysql.js";
+import { UserRouter } from "./v1/router/UserRouter.js";
+import {  ContainerRouter } from "./v1/router/ContainerRouter.js";
+import { SensorRouter } from "./v1/router/SensorRouter.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+createTables();
+
+app.use("/user", UserRouter);
+app.use("/container", ContainerRouter)
+app.use("/sensor", SensorRouter);
+
+app.listen(3000, ()=> {
+    signale.success("Server online in port 3000");
+})
